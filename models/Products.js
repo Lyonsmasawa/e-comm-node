13eb8b6dd4e75299e90a38c274e0592846331ec5
Lyonsmasawa <lyonsmasawa@gmail.com>
2024-03-30 23:46:54 -0700

    schema

--- models/Products.js
+++ models/Products.js
@@ -1,10 +1,14 @@
-const moongoose = require('mongoose');
+const moongoose = require("mongoose");
 
-const ProductSchema = new moongoose.Schema({
-    title: {type: String, required: true},
-    supplier: {type: String, required: true},
-    imageUrl: {type: String, required: true},
-    description: {type: String, required: true},
-    productLocation: {type: String, required: true},
-    title: {type: String, required: true},
-})
+const ProductSchema = new moongoose.Schema(
+  {
+    title: { type: String, required: true },
+    supplier: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    description: { type: String, required: true },
+    product_location: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.export = module.model("Product", ProductSchema);
